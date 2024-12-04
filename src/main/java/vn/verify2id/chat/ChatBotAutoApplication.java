fbe3dc19cefdd95b6b167d1846b5aa51dc8baade
TuanLC <luucungtuan2907@gmail.com>
2024-12-05 01:12:40 +0700

    [CHATBOT-5122024] start project telegram chat bot.

---
+++ src/main/java/vn/verify2id/chat/ChatBotAutoApplication.java
@@ -0,0 +1,13 @@
+package vn.verify2id.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/verify2id/chat/ChatBotAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.verify2id.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
